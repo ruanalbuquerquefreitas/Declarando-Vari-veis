5d9f09d9f79f86185afb37d35d10dd3295ebb6be
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-03-12 15:23:21 +0000

    usando variáveis

---
+++ script.js
@@ -0,0 +1,17 @@
+// var 
+
+//var altura = 5;
+//var comprimento = 7; 
+
+if (forma ==='retângulo')
+//area = altura * comprimento;
+console.log (area);
+ 
+let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+
+area =altura * comprimento;
+} else { 
+     area = (altura * comprimento) / 2;
+}console.log(area)
