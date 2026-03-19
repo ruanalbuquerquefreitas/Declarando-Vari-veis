d7911b7c8ae7c8c809085532d448c72630f5a5b4
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-03-19 14:56:46 +0000

    .

--- script.js
+++ script.js
@@ -16,4 +16,17 @@
 //} else { 
      //area = (altura * comprimento) / 2;
 //}
+
 //console.log(area);
+
+const forma = 'tringulo';
+
+const altura = 5;
+const comprimento = 7; 
+let area;
+if (forma === 'quadrado'){
+     area = altura * comprimento;
+}else{
+     area = (altura * comprimento) /2
+}
+console.log(area);
